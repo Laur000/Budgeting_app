ec3ba51f1d684f31888c5824a52d0d6f676e8269
Laurentiu Bagiu <laurlaurbagiu1.0@yahoo.com>
2022-04-05 09:58:20 +0300

    MainActivity + some logic

--- app/src/main/java/com/example/budgeting_app/BudgetActivity.java
+++ app/src/main/java/com/example/budgeting_app/BudgetActivity.java
@@ -1,14 +1,41 @@
 package com.example.budgeting_app;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class BudgetActivity extends AppCompatActivity {
+
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget);
+
+        fab =findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addItem();
+
+            }
+        });
+    }
+    private void addItem(){
+        AlertDialog.Builder myDialog = new AlertDialog.Builder(this);
+        LayoutInflater inflater =  LayoutInflater.from(this);
+        View myView = inflater.inflate(R.layout.input_layout, null);
+        myDialog.setView(myView);
+
+        final AlertDialog dialog = myDialog.create();
+        dialog.setCancelable(false);
+        dialog.show();
     }
 }
--- app/src/main/java/com/example/budgeting_app/MainActivity.java
+++ app/src/main/java/com/example/budgeting_app/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.budgeting_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CardView budgetCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        budgetCardView = findViewById(R.id.budgetCardView);
+
+        budgetCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this , BudgetActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
